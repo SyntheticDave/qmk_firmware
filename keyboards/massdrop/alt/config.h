f8094baa08f15248e61a793e99d741be1fd10e55
David Reece <dv.reece@gmail.com>
2020-04-05 12:47:31 +0930

    Add SC Shift, Grave Esc, and GPM control

--- keyboards/massdrop/alt/config.h
+++ keyboards/massdrop/alt/config.h
@@ -124,4 +124,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_MATRIX_LED_PROCESS_LIMIT 15
 #define RGB_MATRIX_LED_FLUSH_LIMIT 10
 
+// Always send Escape if CTRL pressed (so CTRL + SHIFT + ESC still works in Windows)
+#define GRAVE_ESC_CTRL_OVERRIDE
+
 #include "config_led.h"
