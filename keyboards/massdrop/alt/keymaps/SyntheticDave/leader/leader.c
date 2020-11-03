2dc7ad8514f277b98dcb08e277ebeffca520ae22
David Reece <dv.reece@gmail.com>
2020-11-03 19:52:36 +1030

    Change double tap leader key to activate base layer
    Wasn't triggering the mac one shot layer with the leader key, and didn't work as a one shot when activated that way.

--- keyboards/massdrop/alt/keymaps/SyntheticDave/leader/leader.c
+++ keyboards/massdrop/alt/keymaps/SyntheticDave/leader/leader.c
@@ -8,24 +8,17 @@ void handle_leader_key(void) {
             // Put the keyboard into bootloading mode for flashing
             reset_keyboard();
         }
-        SEQ_ONE_KEY(KC_0) {
-            // Deactivate all layers except the base layer
-            layer_move(DEFAULT);
-        }
         SEQ_ONE_KEY(KC_M) {
             layer_move(MAC);  // Activate Mac OS Layer (only)
         }
         SEQ_TWO_KEYS(KC_M, KC_M) {
             layer_move(MAC);  // Activate Mac OS Layer (only)
-            toggle_work_mode(true);
+            toggle_work_mode(true); // Toggle work mode - Close work applications if deactivating
         }
         // Double tap leader key
         SEQ_ONE_KEY(KC_LEAD) {
-          // TODO: Consider sending backspace on double lead (has moved to the backspace key)
-          // TODO: Only activate mac oneshot layer if we're on the mac base layer or the osl
-          // layer_state_is(MAC)
-          // Doesn't act like a oneshot layer here, so not particularly useful. I'm not sure what the call to active a layer as a oneshot is, as I can't find it in the docs.
-          layer_invert(MAC_ONE_SHOTS);
+            // Deactivate all layers except the base layer
+            layer_move(DEFAULT);
         }
         SEQ_ONE_KEY(KC_X) {
             toggle_work_mode(false);
